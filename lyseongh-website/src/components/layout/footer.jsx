60e50c89600d8f5e8bd4cd4a95b95eea8831820a
Will <asampongg377@gmail.com>
2025-07-12 13:48:57 +0000

    Edit

--- lyseongh-website/src/components/layout/footer.jsx
+++ lyseongh-website/src/components/layout/footer.jsx
@@ -43,12 +43,11 @@ const FooterColumn = ({ title, links }) => (
     <div className="flex flex-col">
         <div className="title flex flex-col">
             <h3 className="text-2xl font-semibold py-3">{title}</h3>
-            <div  className="w-16 h-[2px] bg-white rounded-full my-2"/>
         </div>
         <ul className="space-y-6 py-3">
             {links.map((link) => (
                 <li className="" key={link.id}>
-                    <Link href={link.href} className="md:text-lg">
+                    <Link href={link.href} className="md:text-lg text-center">
                         {link.name}
                     </Link>
                 </li>
@@ -60,25 +59,24 @@ const FooterColumn = ({ title, links }) => (
 export const ContactInfo = ({ title, className }) => (
     <div className="flex flex-col">
         <div className="flex flex-col py-2">
-            <h3 className="text-3xl font-semibold py-3 ">{title}</h3>
-            <div  className="w-20 border-b-3 rounded-full "/>
+            <h3 className="text-2xl font-semibold py-3 ">{title}</h3>
         </div>
         <div className={className}>
             <div className="flex gap-x-2">
-                <FontAwesomeIcon icon={faLocationDot} className="p-2 flex items-start"/>
+                <FontAwesomeIcon icon={faLocationDot} className="p-1 flex items-start"/>
                 <div>
                     <p>P.O.Box 6017, Accra - North</p>
                     <p>HM8R+722, Top Base Rd, Gbawe, Ghana</p>
                 </div>
             </div>
             <div className="flex gap-x-2 items-center">
-                <FontAwesomeIcon icon={faPhone} className="p-2"/>
+                <FontAwesomeIcon icon={faPhone} className="p-1"/>
                 <div>
                     <p>+233543794580</p>
                 </div>
             </div>
             <div className="flex gap-x-2 items-center">
-                <FontAwesomeIcon icon={faEnvelope} className="p-2"/>
+                <FontAwesomeIcon icon={faEnvelope} className="p-1"/>
                 <div>
                     <p>lyseongh@gmail.com</p>
                 </div>
@@ -91,8 +89,13 @@ export function Footer(){
     return (
         <footer className="bg-[#090922] relative text-white">
             
-            <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-6 py-12">
-                <FooterColumn title="Page Links" links={pageLinks}/>
+            <div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-6 py-12">
+                <div className="">
+                    <h4 className="font-bold text-xl text-white py-3">LYSEONGH</h4>
+                    <p className="text-gray-300 text-md/10 pt-6">LYSEONGH offers  consulting services in Electrical Wiring and Capacity Building <br /> (Bridging the knowledge gap in the above service areas). <br />
+                    Shalom,Peace</p>
+                </div>
+                {/* <FooterColumn title="Page Links" links={pageLinks}/> */}
                 <FooterColumn title="Services" links={services}/>
                 <FooterColumn title="Quick Links" links={quickLinks}/>
                 <div>
@@ -109,7 +112,7 @@ export function Footer(){
                 </div>
             </div>
 
-            <div className="max-w-7xl mx-auto border-t border-white/20 py-6 px-2 flex justify-between text-center text-sm text-white">
+            <div className="mx-auto border-t border-white/20 py-6 px-2 flex justify-between text-center text-sm text-white">
                 <p className="capitealize italic">Lyseon gh</p>
                 <p>All rights reserved. Copyright 2025</p>
             </div>
